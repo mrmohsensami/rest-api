2cb8ed9d68c755f0ea5784e558a081776a532d13
sami <haco.sami@gmail.com>
2022-09-10 11:37:40 +0430

    getposts component added

---
+++ src/components/Basic.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button @click="handleClick">get text from file</button>
+  <p>
+    {{ text }}
+  </p>
+  <button @click="getUserData">get user info from file</button>
+  <p>name is: {{ user.name }}</p>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  props: ['msg'],
+
+  setup() {
+    const text = ref('خالی');
+    const user = reactive({ legs: 2 });
+
+    const handleClick = () => {
+
+      fetch('/text.txt')
+        .then(response => response.text())
+        .then(message => text.value = message);
+    };
+
+    const getUserData = () => {
+      fetch('/user.json')
+        .then(res => res.json())
+        .then(data => Object.assign(user, data))
+      
+      // console.log('user fetching...')
+    }
+
+    return {
+      text,
+      user,
+      handleClick,
+      getUserData
+    };
+  }
+
+  // data: () => ({
+  //   text: 'empty'
+  // }),
+  // methods: {
+  //   handleClick() {
+  //     fetch('/text.txt')
+  //       .then(response => {
+  //         return response.text();
+  //       })
+  //       .then(message => {
+  //         this.text = message
+  //       })
+  //   },
+  //   getUserData() {
+  //     fetch('/user.json')
+  //       .then(res => res.json())
+  //       .then(data => this.user = data)
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>{{ msg }}</h1>
+  
+    <button @click="getPosts">get posts from jsonplaceholder</button>
+    <p v-for="post in posts" :key="post.id">
+      {{ post.id }} post : {{ post.title }}
+    </p>
+  </template>
+  
+  <script>
+  import { ref, reactive } from "vue";
+  
+  export default {
+    name: "Posts",
+  
+    setup() {
+      const posts = reactive([]);
+  
+      const getPosts = () => {
+        
+        fetch('https://jsonplaceholder.typicode.com/posts')
+          .then(res => res.json())
+          .then(data => posts.push(...data))
+      }
+  
+      return {
+        posts,
+        getPosts
+      };
+    },
+  
+    /* data: () => ({
+      posts: []
+    }),
+    methods: {
+      getPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+          .then(res => res.json())
+          .then(data => this.posts = data)
+      }
+    } */
+  };
+  </script>
+  
+  <style scoped>
+  a {
+    color: #42b983;
+  }
+  </style>
